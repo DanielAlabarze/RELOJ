898bd31cb23cee932ba9c5d59eaa4b16256fa013
DanielAlabarze <139582202+DanielAlabarze@users.noreply.github.com>
2025-01-02 22:31:47 -0300

    Se agrego Reloj Digital

--- index.js
+++ index.js
@@ -3,17 +3,8 @@ const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
 const day = document.querySelector(".dia");
 const nombreDia = document.querySelector(".nombreDia");
-
-const RELOJ = () => {
-  const DATE = new Date();
-  const SEGUNDOS = DATE.getSeconds() / 60;
-  const MINUTOS = (SEGUNDOS + DATE.getMinutes()) / 60;
-  const HORAS = (MINUTOS + DATE.getHours()) / 12;
-
-  seconds.style.setProperty("--rotate", SEGUNDOS * 360);
-  minutes.style.setProperty("--rotate", MINUTOS * 360);
-  hours.style.setProperty("--rotate", HORAS * 360);
-};
+const horaDigital = document.querySelector(".horaDigital");
+const pajaroCucu = document.querySelector(".cucu");
 
 const FECHA = new Date();
 const nDIA = FECHA.getDate();
@@ -34,9 +25,62 @@ let diaDeSemana = FECHA.getDay();
 
 let nombreDelDia = nombresDias[diaDeSemana];
 
-day.innerText =
-  nDIA.toString() + " - " + MES.toString() + " - " + ANO.toString();
-
 nombreDia.innerHTML = nombreDelDia;
 
+day.innerHTML = `${nDIA < 10 ? "0" + nDIA : nDIA} - ${
+  MES < 10 ? "0" + MES : MES
+} - ${ANO}`;
+
+/*function reproducirAudio() {
+  audio.play(); // Reproduce el sonido
+  setTimeout(() => {
+    audio.pause(); // Pausa el audio después de 80ms
+    audio.currentTime = 0; // Reinicia el audio al inicio
+  }, 1000);
+};
+
+ reproducirAudio()*/
+
+const RELOJ = () => {
+  const DATE = new Date();
+  const SEGUNDOS = DATE.getSeconds() / 60;
+  const MINUTOS = (SEGUNDOS + DATE.getMinutes()) / 60;
+  const HORAS = (MINUTOS + DATE.getHours()) / 12;
+
+  seconds.style.setProperty("--rotate", SEGUNDOS * 360);
+  minutes.style.setProperty("--rotate", MINUTOS * 360);
+  hours.style.setProperty("--rotate", HORAS * 360);
+};
+
 setInterval(RELOJ, 1000);
+
+const RelojDigital = () => {
+  const DATE = new Date();
+  const HORA = DATE.getHours();
+  const MINUTO = DATE.getMinutes();
+  const SEGUNDO = DATE.getSeconds();
+
+  horaDigital.innerHTML = `${HORA < 10 ? "0" + HORA : HORA}:${
+    MINUTO < 10 ? "0" + MINUTO : MINUTO
+  }:${SEGUNDO < 10 ? "0" + SEGUNDO : SEGUNDO}`;
+};
+
+setInterval(RelojDigital, 1000);
+
+const CUCU = () => {
+  const DATE = new Date();
+  const SEGUNDOS = DATE.getSeconds() / 60;
+  const MINUTOS = (SEGUNDOS + DATE.getMinutes()) / 60;
+  const HORAS = (MINUTOS + DATE.getHours()) / 12;
+  if (SEGUNDOS === 0) {
+    pajaroCucu.style.display = "block";
+    const audio = new Audio("./audio/cardinal-37075.mp3");
+    audio.play().catch((error) => {
+      console.error("Error al reproducir el audio:", error);
+    });
+  } else {
+    pajaroCucu.style.display = "none";
+  }
+};
+
+setInterval(CUCU, 1000);
